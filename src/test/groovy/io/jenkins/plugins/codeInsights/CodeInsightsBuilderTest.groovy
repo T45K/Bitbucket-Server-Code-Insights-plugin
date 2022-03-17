59191a06ec1e7aa87cece48055455b7b6a8da0f4
T45K <tasktas9@gmail.com>
2022-03-17 18:47:11 +0900

    add test

--- src/test/groovy/io/jenkins/plugins/codeInsights/CodeInsightsBuilderTest.groovy
+++ src/test/groovy/io/jenkins/plugins/codeInsights/CodeInsightsBuilderTest.groovy
@@ -1,6 +1,5 @@
 package io.jenkins.plugins.codeInsights
 
-import hudson.model.Result
 import net.sf.json.JSONObject
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
@@ -9,6 +8,8 @@ import org.jvnet.hudson.test.JenkinsRule
 import org.kohsuke.stapler.StaplerRequest
 import spock.lang.Specification
 
+import java.nio.file.Paths
+
 class CodeInsightsBuilderTest extends Specification {
 
     @Rule
@@ -16,7 +17,7 @@ class CodeInsightsBuilderTest extends Specification {
 
     final def server = new MockWebServer()
 
-    def jsonObject = new JSONObject(
+    final def jsonObject = new JSONObject(
         [codeInsights: [
             bitbucketUrl: server.url('').toString(),
             project     : 'project',
@@ -24,6 +25,8 @@ class CodeInsightsBuilderTest extends Specification {
             username    : 'username',
             password    : 'password'
         ]])
+
+    final def INITIAL_COMMIT_ID = 'ed5582899d97af2ec47dad0462a7a38a8f3ebeeb'
 
     def 'test config round-trip'() {
         given:
@@ -43,14 +46,16 @@ class CodeInsightsBuilderTest extends Specification {
         server.enqueue(new MockResponse().setResponseCode(200))
 
         def project = jenkins.createFreeStyleProject()
-        def builder = new CodeInsightsBuilder('repo', 'src/main/java', '0' * 40)
+        def builder = new CodeInsightsBuilder('repo', 'src/main/java', INITIAL_COMMIT_ID)
         project.buildersList << builder
-        project.customWorkspace = '.'
+        project.customWorkspace = Paths.get(".").toAbsolutePath().toString()
         jenkins.get(CodeInsightsBuilder.DescriptorImpl).configure(Stub(StaplerRequest), jsonObject)
 
         expect:
-        def build = jenkins.buildAndAssertStatus(Result.FAILURE, project)
+        def build = jenkins.buildAndAssertSuccess(project)
         jenkins.assertLogContains('[Code Insights plugin] Start to put reports', build)
+        jenkins.assertLogContains('[Code Insights plugin] Put reports: SUCCESS', build)
+        jenkins.assertLogContains('Finished: SUCCESS', build)
         jenkins.assertLogNotContains('[Code Insights plugin] Checkstyle enabled', build)
     }
 
