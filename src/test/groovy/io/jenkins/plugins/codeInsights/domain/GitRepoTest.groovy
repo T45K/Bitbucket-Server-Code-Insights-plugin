fe43d32c74de212ae3fb1007cc1b74033290aa43
T45K <tasktas9@gmail.com>
2022-03-22 09:02:17 +0900

    fix master commit

--- src/test/groovy/io/jenkins/plugins/codeInsights/domain/GitRepoTest.groovy
+++ src/test/groovy/io/jenkins/plugins/codeInsights/domain/GitRepoTest.groovy
@@ -1,7 +1,6 @@
 package io.jenkins.plugins.codeInsights.domain
 
 import io.jenkins.plugins.codeInsights.usecase.GitRepo
-import spock.lang.IgnoreIf
 import spock.lang.Specification
 
 class GitRepoTest extends Specification {
@@ -10,7 +9,7 @@ class GitRepoTest extends Specification {
         final def sut = new GitRepo('.git')
 
         expect:
-        sut.detectChangedFiles('661878933afdc83670558234f9ed6a0ce6084794', 'master') ==~ [
+        sut.detectChangedFiles('661878933afdc83670558234f9ed6a0ce6084794', 'a05b196dac972561f0d9456a9a5b95cd471d2337') ==~ [
             'src/test/resources/gitrepo-modified',
             'src/test/resources/gitrepo-added'
         ]
