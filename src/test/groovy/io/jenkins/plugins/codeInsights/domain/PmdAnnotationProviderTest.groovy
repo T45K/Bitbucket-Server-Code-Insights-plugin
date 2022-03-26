941ab2ac57986ad9b6cba47e2829a8cbd6e3e32d
T45K <tasktas9@gmail.com>
2022-03-26 18:41:53 +0900

    add test

--- src/test/groovy/io/jenkins/plugins/codeInsights/domain/PmdAnnotationProviderTest.groovy
+++ src/test/groovy/io/jenkins/plugins/codeInsights/domain/PmdAnnotationProviderTest.groovy
@@ -6,6 +6,7 @@ import spock.lang.Specification
 
 import java.nio.file.Paths
 
+@SuppressWarnings('GroovyAccessibility')
 class PmdAnnotationProviderTest extends Specification {
 
     def 'convert returns annotation based on PMD result file'() {
@@ -21,5 +22,26 @@ class PmdAnnotationProviderTest extends Specification {
             new Annotation(19, 'PMD says: Avoid unused local variables such as \'a\'.',
                 'src/main/java/io/jenkins/plugins/codeInsights/Sample.java', Severity.MEDIUM, 'https://pmd.github.io/pmd-6.42.0/pmd_rules_java_bestpractices.html#unusedlocalvariable')
         ]
+    }
+
+    def 'toAnnotationSeverity converts int value to Severity'() {
+        given:
+        final def sut = new PmdAnnotationProvider(
+            new XmlMapper(),
+            '/test/repo',
+            FileUtil.readString(Paths.get('src', 'test', 'resources', 'pmd-test.xml')),
+        )
+
+        expect:
+        sut.toAnnotationSeverity(input) == output
+
+        where:
+        input || output
+        1     || Severity.HIGH
+        2     || Severity.MEDIUM
+        3     || Severity.MEDIUM
+        4     || Severity.LOW
+        5     || Severity.LOW
+        10000 || Severity.LOW
     }
 }
